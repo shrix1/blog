2b998ee77ee34fedfe84467ffb297ae5cc91ab8c
Shriprasanna <shriprasanna32@gmail.com>
2023-07-17 14:09:53 +0530

    try deploy ment #2

--- app/all-blog/[name]/page.tsx
+++ app/all-blog/[name]/page.tsx
@@ -1,7 +1,7 @@
-// import React from "react"
+import React from "react"
 // import UserInfo from "./UserInfo"
 // import BlogOverview from "./BlogOverview"
-// import { PropsBlogData } from "@/types/props"
+import { PropsBlogData } from "@/types/props"
 
 // const getCorrectBlogs = async () => {
 //   try {
@@ -15,22 +15,22 @@
 //   }
 // }
 
-// const Page = async ({ params }: { params: { name: string } }) => {
-//   const { name } = params
-//   const { blogs } = await getCorrectBlogs()
+const Page = async ({ params }: { params: { name: string } }) => {
+  // const { name } = params
+  // const { blogs } = await getCorrectBlogs()
 
-//   const filteredData = blogs.filter(
-//     (item: PropsBlogData) =>
-//       item.title.replace(/[^\w]/gi, "-").replaceAll(" ", "-") === name
-//   )
+  // const filteredData = blogs.filter(
+  //   (item: PropsBlogData) =>
+  //     item.title.replace(/[^\w]/gi, "-").replaceAll(" ", "-") === name
+  // )
 
-//   return (
-//     <section className="w-full h-[90vh] flex flex-col gap-5 items-center py-8 px-10 lg:px-0">
-//       <UserInfo data={filteredData} />
-//       <hr className="w-full lg:w-1/2" />
-//       <BlogOverview data={filteredData} />
-//     </section>
-//   )
-// }
+  return (
+    <section className="w-full h-[90vh] flex flex-col gap-5 items-center py-8 px-10 lg:px-0">
+      {/* <UserInfo data={filteredData} />
+      <hr className="w-full lg:w-1/2" />
+      <BlogOverview data={filteredData} /> */}
+    </section>
+  )
+}
 
-// export default Page
+export default Page
--- app/all-blog/page.tsx
+++ app/all-blog/page.tsx
@@ -1,8 +1,8 @@
-// import BlogCard from "@/components/filter/BlogCard"
-// import Filter from "@/components/filter/Filter"
-// import React from "react"
-// import Greeting from "@/components/filter/Greeting"
-// import { PropsBlogData } from "@/types/props"
+import BlogCard from "@/components/filter/BlogCard"
+import Filter from "@/components/filter/Filter"
+import React from "react"
+import Greeting from "@/components/filter/Greeting"
+import { PropsBlogData } from "@/types/props"
 
 // const getBlogs = async () => {
 //   try {
@@ -17,28 +17,28 @@
 //   }
 // }
 
-// const Page = async () => {
-//   const { blogs } = await getBlogs()
+const Page = async () => {
+  // const { blogs } = await getBlogs()
 
-//   return (
-//     <main className="w-full flex py-4 px-6 items-center h-[90vh] flex-col gap-5 ">
-//       <Greeting />
-//       <Filter />
+  return (
+    <main className="w-full flex py-4 px-6 items-center h-[90vh] flex-col gap-5 ">
+      {/* <Greeting />
+      <Filter />
 
-//       <hr className="w-full lg:w-1/2" />
+      <hr className="w-full lg:w-1/2" />
 
-//       {blogs.map((item: PropsBlogData, index: React.Key | null | undefined) => {
-//         return (
-//           <div
-//             key={index}
-//             className="grid place-items-center flex-col w-full gap-3"
-//           >
-//             <BlogCard data={item} />
-//           </div>
-//         )
-//       })}
-//     </main>
-//   )
-// }
+      {blogs.map((item: PropsBlogData, index: React.Key | null | undefined) => {
+        return (
+          <div
+            key={index}
+            className="grid place-items-center flex-col w-full gap-3"
+          >
+            <BlogCard data={item} />
+          </div>
+        )
+      })} */}
+    </main>
+  )
+}
 
-// export default Page
+export default Page
