84486b74fdbc48aebbaa515e12065d38c5159bad
Shriprasanna <shriprasanna32@gmail.com>
2023-07-18 00:39:21 +0530

    refactor : next-auth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,20 @@
 import NextAuth from "next-auth"
-import { options } from "./options"
+import type { NextAuthOptions } from "next-auth"
+import GitHubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+
+const options: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+}
 
 const handler = NextAuth(options)
 
