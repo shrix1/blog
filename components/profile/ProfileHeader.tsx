76a21f68f297f7597b0027c6de5f06239c202a42
Shriprasanna <shriprasanna32@gmail.com>
2023-07-18 15:26:25 +0530

    fix : user profile img in blog route

--- components/profile/ProfileHeader.tsx
+++ components/profile/ProfileHeader.tsx
@@ -1,7 +1,7 @@
+"use client"
 import React from "react"
 import { Avatar, AvatarFallback } from "../ui/avatar"
 import { AvatarImage } from "@radix-ui/react-avatar"
-import { useSession } from "next-auth/react"
 import { PropsBlogData } from "@/types/props"
 
 const ProfileHeader = ({ filteredUser }: PropsBlogData[] | any) => {
@@ -9,12 +9,12 @@ const ProfileHeader = ({ filteredUser }: PropsBlogData[] | any) => {
 
   return (
     <section className="w-full lg:w-1/2 flex justify-center flex-col gap-3 items-center">
-        <Avatar className="w-24 h-24">
-          <AvatarImage src={img} alt="user profile picture" />
-          <AvatarFallback>
-            {user && user.slice(0, 2).toUpperCase()}
-          </AvatarFallback>
-        </Avatar>
+      <Avatar className="w-24 h-24">
+        <AvatarImage src={img} alt="user profile picture" />
+        <AvatarFallback>
+          {user && user.slice(0, 2).toUpperCase()}
+        </AvatarFallback>
+      </Avatar>
 
       <div className="flex flex-col justify-center items-center">
         <h1 className="capitalize text-xl font-semibold">{user}</h1>
