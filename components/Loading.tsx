19c462aea9e4b572fda116ab14ad252801fb064a
Shriprasanna <shriprasanna32@gmail.com>
2023-08-01 23:45:49 +0530

    fix : Loading components

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Loading = ({ text }: { text: string }) => {
   return (
-    <section className="fixed -mt-14 flex justify-center items-center w-full h-screen z-[100] dark:bg-white/10 bg-black/20 ">
+    <section className="fixed -mt-14 flex justify-center items-center w-full h-screen z-[100] dark:bg-white/10 bg-black/10 backdrop-blur-[2px] ">
       <div
         className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-b-black/70
         border-t-black dark:border-b-white/70 grid place-items-center dark:border-t-white"
