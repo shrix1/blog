f518be725b2b365964d6b0478e48a2109c5e3f0e
Shriprasanna <shriprasanna32@gmail.com>
2023-10-29 13:02:27 +0530

    Add Og Image for root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,24 @@ import AuthProvider from "@/context/AuthProvider"
 
 const inter = Inter({ subsets: ["latin"] })
 
+const title = "Blog it!"
+const description =
+  "Blog it! is a platform for individuals to share their unique perspectives, meaningful stories with the world."
+
 export const metadata: Metadata = {
-  title: "Blog it!",
-  description:
-    "Blog it! is a platform for individuals to share their unique perspectives, meaningful stories with the world.",
+  metadataBase: new URL("https://blog-shrix1.vercel.app/"),
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+  },
+  twitter: {
+    title,
+    description,
+    card: "summary_large_image",
+    creator: "@shriprasanna007",
+  },
 }
 
 export default function RootLayout({
