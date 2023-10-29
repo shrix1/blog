8af483fde55e2f6eb4c5f57918eda30e606c03f8
Shriprasanna <shriprasanna32@gmail.com>
2023-10-29 15:09:24 +0530

    Rm Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,24 +11,24 @@ const title = "Blog it!"
 const description =
   "Blog it! is a platform for individuals to share their unique perspectives, meaningful stories with the world."
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://blog-shrix1.vercel.app"),
-  title,
-  description,
-  openGraph: {
-    title,
-    description,
-  },
-  alternates: {
-    canonical: "/",
-  },
-  twitter: {
-    title,
-    description,
-    card: "summary_large_image",
-    creator: "@shriprasanna007",
-  },
-}
+// export const metadata: Metadata = {
+//   metadataBase: new URL("https://blog-shrix1.vercel.app"),
+//   title,
+//   description,
+//   openGraph: {
+//     title,
+//     description,
+//   },
+//   alternates: {
+//     canonical: "/",
+//   },
+//   twitter: {
+//     title,
+//     description,
+//     card: "summary_large_image",
+//     creator: "@shriprasanna007",
+//   },
+// }
 
 export default function RootLayout({
   children,
